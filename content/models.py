026d9b62eb356cc162ab95545906945589cde461
Uladzislaw <uladzislawchyrets@gmail.com>
2025-12-24 22:41:31 +0300

    split post model

--- content/models.py
+++ content/models.py
@@ -1,10 +1,24 @@
 from sqlmodel import Field, SQLModel
 
 
-class Post(SQLModel, table=True):
-    id: int | None = Field
+class BasePost(SQLModel):
     title: str = Field(index=True)
     content: str = Field()
     category: str | None = Field(default=None)
     tags: list[str] | None = Field(default=None)
+
+
+class Post(BasePost, table=True):
+    id: int | None = Field
+
+
+class CreatePost(BasePost):
+    pass
+
+
+class UpdatePost(BasePost):
+    title: str | None = None
+    content: str | None = None
+    category: str | None = None
+    tags: list[str] | None = None
     
