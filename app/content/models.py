2a92445a6022fd34707f7ed841b4b17b5824e0ac
Uladzislaw <uladzislawchyrets@gmail.com>
2026-01-14 19:17:09 +0300

    added new fields for post

--- app/content/models.py
+++ app/content/models.py
@@ -1,4 +1,5 @@
-from sqlmodel import Field, Relationship, SQLModel
+from datetime import datetime
+from sqlmodel import DateTime, Field, Relationship, SQLModel, Column, func
 
 from app.users.models import User, UserPublic
 
@@ -12,6 +13,19 @@ class BasePost(SQLModel):
 
 class Post(BasePost, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    created_at: datetime | None = Field(
+        sa_column=Column(
+            DateTime(timezone=True), server_default=func.now(), nullable=True
+        )
+    )
+    updated: bool = Field(default=False)
+    updated_at: datetime | None = Field(
+        sa_column=Column(
+            DateTime(timezone=True), onupdate=func.now(), nullable=True
+        )
+    )
+    deleted: bool = Field(default=False)
+    deleted_at: datetime | None = Field(default=None)
     
     author_id: int = Field(foreign_key="user.id")
     author: User = Relationship(back_populates="posts")
